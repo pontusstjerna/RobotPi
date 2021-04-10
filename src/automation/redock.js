37a9582104dbee8e1a91cac8e4e626249de5bea3
Pontus Stjernström <pontus@stjernstrom.nu>
2021-04-10 23:30:17 +0200

    Adjust redocking times and power

--- src/automation/redock.js
+++ src/automation/redock.js
@@ -11,25 +11,25 @@ export default () => {
 const redock = () =>
   new Promise(resolve => {
     const oldPower = controller.getPower()
-    controller.setPower(0.2)
+    controller.setPower(0.1)
     controller.reverse()
 
-    forOneSecond(controller.forward)
+    for500ms(controller.forward)
       .then(() =>
-        forOneSecond(() => {
+        for500ms(() => {
           controller.reverse()
           controller.forward()
         })
       )
-      .then(() => forOneSecond(controller.stop))
+      .then(() => for500ms(controller.stop))
       .then(() => controller.setPower(oldPower))
       .then(resolve())
   })
 
-const forOneSecond = action =>
+const for500ms = action =>
   new Promise(resolve =>
     setTimeout(() => {
       action()
       resolve()
-    }, 1000)
+    }, 500)
   )
