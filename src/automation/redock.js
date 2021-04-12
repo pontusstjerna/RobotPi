5d59bc45cad84ef0e330b5077a60f212af1eded5
Pontus Stjernström <pontus@stjernstrom.nu>
2021-04-12 10:45:28 +0200

    Change time of redock

--- src/automation/redock.js
+++ src/automation/redock.js
@@ -11,27 +11,29 @@ export default () => {
 const redock = () =>
   new Promise(resolve => {
     const oldPower = controller.getPower()
-    controller.setPower(0.13)
+    controller.setPower(0.12)
     controller.reverse()
 
-    forOneSecond(controller.forward)
+    momentarilyDo(controller.forward)
       .then(() =>
-        forOneSecond(() => {
+        momentarilyDo(() => {
           controller.reverse()
           controller.forward()
         })
       )
-      .then(() => forOneSecond(() => {
-        controller.stop()
-        controller.setPower(oldPower)
-      }))
+      .then(() =>
+        momentarilyDo(() => {
+          controller.stop()
+          controller.setPower(oldPower)
+        })
+      )
       .then(resolve)
   })
 
-const forOneSecond = action =>
+const momentarilyDo = action =>
   new Promise(resolve =>
     setTimeout(() => {
       action()
       resolve()
-    }, 1000)
+    }, 750)
   )
