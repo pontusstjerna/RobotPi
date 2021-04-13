db0189891cf3154e12b62844664a9aec7228e44c
pontusstjerna <pontus@stjernstrom.nu>
2021-04-13 19:02:11 +0000

    Tuned and fixed ppython servo controller

--- python/servo_controller.py
+++ python/servo_controller.py
@@ -3,11 +3,11 @@ import time
 import threading
 
 pin_number = 14
-max_cycle = 12.5
-min_cycle = 2.5
+max_cycle = 12
+min_cycle = 3
 frequency = 50
-sleep_s = 0.2
-servo_speed = 0.1
+sleep_s = 0.02
+servo_speed = 0.05
 
 # Set mode to GPIO numbers (not PIN numbers) 
 io.setwarnings(False)
@@ -18,11 +18,12 @@ io.setup(pin_number, io.OUT)
 pwm = io.PWM(pin_number, frequency)
 
 current_speed = 0.0
-current_cycle = max_cycle - (max_cycle - min_cycle) * 0.5
+current_cycle = 7.5
 
 pwm.start(0)
 
 def exec_servo():
+  global current_cycle
   current_cycle = max(min_cycle, min(max_cycle, current_cycle + current_speed))
   pwm.ChangeDutyCycle(current_cycle)
 
@@ -34,15 +35,18 @@ def exec_servo():
     threading.Timer(sleep_s, exec_servo).start()
 
 def increase_angle():
+  global current_speed
   current_speed = servo_speed
   exec_servo()
 
 def decrease_angle():
+  global current_speed
   current_speed = -servo_speed
   exec_servo()
 
 def stop():
-  speed = 0
+  global current_speed
+  current_speed = 0
   pwm.ChangeDutyCycle(0)
 
 def exit():
