8f6d60014be4d58a40f442399ffdc2b3e9f102d1
pontusstjerna <pontus@stjernstrom.nu>
2021-04-13 18:26:32 +0000

    Cleanup in python servo controller

--- python/servo_controller.py
+++ python/servo_controller.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as io
 import time
-from enum import Enum
 
 pin_number = 14
 max_cycle = 12.5
@@ -21,13 +20,12 @@ current_cycle = max_cycle - (max_cycle - min_cycle) * 0.5
 
 pwm.start(0)
 
-while True:
-  if current_speed != 0:
-    current_cycle = max(min_cycle, min(max_cycle, current_cycle + current_speed))
-    pwm.ChangeDutyCycle(current_cycle)
-    time.sleep(sleep_s)
-    if current_cycle == min_cycle or current_cycle == max_cycle:
-      stop()
+while current_speed != 0:
+  current_cycle = max(min_cycle, min(max_cycle, current_cycle + current_speed))
+  pwm.ChangeDutyCycle(current_cycle)
+  time.sleep(sleep_s)
+  if current_cycle == min_cycle or current_cycle == max_cycle:
+    stop()
 
 def increase_angle():
   current_speed = servo_speed
