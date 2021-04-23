b0b89caf45adca6b7065052c93fdf2031059d01b
Pontus Stjernström <pontus@stjernstrom.nu>
2021-04-23 15:01:11 +0200

    Try to update to new gpio lib for pwm pulses to sero

--- python/servo_controller.py
+++ python/servo_controller.py
@@ -1,36 +1,33 @@
 import RPi.GPIO as io
+import pigpio
 import time
 import threading
 
 pin_number = 14
-max_cycle = 12
-min_cycle = 3
-frequency = 50
+max_cycle = 2000
+min_cycle = 1000
 sleep_s = 0.02
-servo_speed = 0.05
+servo_speed = 10
 
 # Set mode to GPIO numbers (not PIN numbers) 
 io.setwarnings(False)
 io.setmode(io.BCM)
 io.setup(pin_number, io.OUT)
 
-# 50Hz for servo
-pwm = io.PWM(pin_number, frequency)
-
 current_speed = 0.0
-current_cycle = 7.5
+current_cycle = 1500
 
-pwm.start(0)
+pi = pigpio.pi()
 
 def exec_servo():
   global current_cycle
   current_cycle = max(min_cycle, min(max_cycle, current_cycle + current_speed))
-  pwm.ChangeDutyCycle(current_cycle)
+  pi.set_servo_pulsewidth(pin_number, current_cycle)
 
   if current_cycle == min_cycle or current_cycle == max_cycle:
     stop()
 
-  # Run this function again after sleep_s seconds
+  # Run this function again after sleep_s seconds if we haven't stopped
   if current_speed != 0:
     threading.Timer(sleep_s, exec_servo).start()
 
@@ -47,7 +44,7 @@ def decrease_angle():
 def stop():
   global current_speed
   current_speed = 0
-  pwm.ChangeDutyCycle(0)
+  pi.set_servo_pulsewidth(pin_number, 0)
 
 def exit():
   pwm.stop()
