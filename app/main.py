8810f8f428eacbed4d3ab4b7bd9ab6b71df870c4
pontusstjerna <pontus.drejer@gmail.com>
2022-01-11 14:59:07 +0100

    Implement first part of new python code

---
+++ app/main.py
@@ -0,0 +1,51 @@
+from dotenv import load_dotenv
+import json
+from mqtt_client import MqttClient
+import os
+import sys
+from datetime import datetime, timedelta
+import time
+
+load_dotenv()
+
+is_debug = os.environ.get("DEBUG") or sys.argv[1] == "nopi"
+idle_timeout_s = 60 * 5
+
+class RobotPi:
+    def __init__(self):
+        self.started = datetime.now()
+        self.last_connected = self.started
+        self.is_running = False
+        self.mqtt_client = MqttClient(on_message = self.on_message)
+
+    def on_message(self, message):
+        if not self.is_running:
+            print("Starting video stream.")
+            self.is_running = True
+
+        if message == "started":
+            self.mqtt_client.publish_message("started", message=json.dumps({
+                "started": self.started.strftime("%Y-%m-%d %H:%M:%S"),
+                "lastConnected": self.last_connected.strftime("%Y-%m-%d %H:%M:%S"),
+            }))
+            self.last_connected = datetime.now()
+
+    def startup(self):
+        self.mqtt_client.connect()
+        try:
+            while True:
+                time.sleep(10)
+                if self.is_running and datetime.now() - self.last_connected > timedelta(seconds=idle_timeout_s):
+                    self.is_running = False
+                    print("Timeout, stopping video stream.")
+
+        except KeyboardInterrupt:
+            print("Exiting...")
+            pass
+        
+        self.mqtt_client.disconnect()
+
+        
+print(f"Robotpi starting up with debug set to {is_debug}")
+RobotPi().startup()
+
---
+++ app/mqtt_client.py
@@ -0,0 +1,45 @@
+import os
+from paho.mqtt import client as mqtt_client
+
+class MqttClient:
+
+    def __init__(self, on_message):
+        self.topic = "robotpi"
+        self.on_message = on_message
+
+    def connect(self):
+        url = os.environ["MQTT_BROKER_URL"]
+        username = os.environ["MQTT_USERNAME"]
+        password = os.environ["MQTT_PASSWORD"]
+        client_id = "robotpi-mqtt-client"
+
+
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                print(f"Successfully connected to MQTT broker {url}")
+            else:
+                print(f"Failed to connect, return code {rc}")
+
+        client = mqtt_client.Client(client_id)
+        client.username_pw_set(username, password)
+        client.on_connect = on_connect
+        client.on_message = self.on_message_received
+        client.connect(url)
+
+        client.loop_start()
+        client.subscribe(self.topic)
+        self.client = client
+
+    def disconnect(self):
+        self.client.loop_stop()
+
+    def publish_message(self, subtopic, message):
+        result = self.client.publish(f"{self.topic}/{subtopic}", message)
+        if result[0] != 0:
+            print("Failed to send message!")
+
+    def on_message_received(self, topic, userdata, msg):
+        decoded_message = msg.payload.decode()
+        print(f"Received {decoded_message} from topic {msg.topic}")
+        
+        self.on_message(decoded_message)
