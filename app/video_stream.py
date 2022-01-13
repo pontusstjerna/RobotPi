a07499c03db244b1a139abafdd7a4d94d95ddb60
Pontus Drejer <pontus.drejer@ri.se>
2022-01-13 08:28:08 +0100

    Change subprocess spawn method

--- app/video_stream.py
+++ app/video_stream.py
@@ -6,26 +6,8 @@ def start_video_stream_process():
     video_stream_host = os.environ.get("VIDEO_STREAM_HOST")
     if video_stream_host:
         print("Starting video stream.")
-        return subprocess.run(
-            [
-                "exec",
-                "ffmpeg",
-                "-s",
-                "640x480",
-                "-f",
-                "video4linux2",
-                "-i",
-                "/dev/video0",
-                "-f",
-                "mpegts",
-                "-codec:v",
-                "mpeg1video",
-                "-codec:a",
-                "mp2",
-                "-b",
-                "1000k",
-                f"{video_stream_host}/video_stream/robotpi",
-            ],
+        return subprocess.Popen(
+            f"exec ffmpeg -s 640x480 -f video4linux2 -i /dev/video0 -f mpegts -codec:v mpeg1video -codec:a mp2 -b 1000k {video_stream_host}/video_stream/robotpi",
             stdout=subprocess.PIPE,
             shell=True,
         )
