e52ca73d91d8af5fb6bf03ff22ed70fcecc1d364
Pontus Drejer <pontus.drejer@ri.se>
2022-01-14 17:28:54 +0100

    Start on returning status object

--- get_status.sh
+++ get_status.sh
@@ -1,6 +1,6 @@
-/opt/vc/bin/vcgencmd get_throttled
-/opt/vc/bin/vcgencmd measure_temp
-/opt/vc/bin/vcgencmd measure_volts core
-/opt/vc/bin/vcgencmd measure_volts sdram_c
-/opt/vc/bin/vcgencmd measure_volts sdram_i
-/opt/vc/bin/vcgencmd measure_volts sdram_p
+vcgencmd get_throttled
+vcgencmd measure_temp
+vcgencmd measure_volts core
+vcgencmd measure_volts sdram_c
+vcgencmd measure_volts sdram_i
+vcgencmd measure_volts sdram_p
