29c8639ec1589412d949849b31c25c2108cbacaf
Pontus Drejer <pontus.drejer@ri.se>
2022-01-24 15:20:06 +0100

    Only decode first time

--- app/qr_code_follower.py
+++ app/qr_code_follower.py
@@ -3,8 +3,10 @@ import os
 import cv2
 from controller import set_motors
 
+
 def get_dist(a, b):
     return isqrt(int(pow(b[0] - a[0], 2)) + int(pow(b[1] - a[1], 2)))
+
 
 qr_dock_text = os.environ.get("QR_DOCK_TEXT") or "robotpi-dock"
 qr_follow_text = os.environ.get("QR_FOLLOW_TEXT") or "robotpi-follow"
@@ -12,8 +14,10 @@ follow_proximity_margin = 50
 stop_diagonal_len = 150
 min_diagonal_len = 50
 
+
 class QrCodeFollower:
     running = False
+    found_qr = False
     diagonal_len = -1
 
     def start(self):
@@ -25,17 +29,26 @@ class QrCodeFollower:
 
         # QR code detection object
         detector = cv2.QRCodeDetector()
-    
+
         while self.running:
             # get the image
             _, img = cap.read()
-            # get bounding box coords and data
-            data, bounding_box, _ = detector.detectAndDecode(img)
+
+            bounding_box = None
+
+            if not self.found_qr:
+                # get bounding box coords and data
+                # data, bounding_box, _ = detector.detectAndDecode(img)
+                data, _, _ = detector.detectAndDecode(img)
+
+                self.found_qr = data == qr_dock_text or data == qr_follow_text
+            else:
+                bounding_box, _ = detector.detect(img)
 
             height, width, _ = img.shape
             max_diag = get_dist([0, 0], [width, height])
 
-            #if there is a bounding box, draw one, along with the data
+            # if there is a bounding box, draw one, along with the data
             if bounding_box is not None and data is not None:
                 points = bounding_box[0]
                 top_left_corner = points[0]
@@ -43,41 +56,28 @@ class QrCodeFollower:
                 diagonal_len = get_dist(points[0], points[2])
 
                 portion_of_img = diagonal_len / max_diag
-                
+
                 print(f"Data: {data}")
-                #print(f"Center point: {top_left_corner}")
-                #print(f"Width: {width}, Height: {height}")
+                # print(f"Center point: {top_left_corner}")
+                # print(f"Width: {width}, Height: {height}")
 
-                print(f"{'top' if top_left_corner[1] < height / 2 else 'bottom'} {'left' if top_left_corner[0] < width / 2 else 'right'}")
-                
+                print(
+                    f"{'top' if top_left_corner[1] < height / 2 else 'bottom'} {'left' if top_left_corner[0] < width / 2 else 'right'}"
+                )
 
-                if data == qr_dock_text or data == qr_follow_text:
-                    pwr = portion_of_img
-                    print(f"diag_len: {diagonal_len}")
-                    print(f"Pwr: {pwr}")
-                    if top_left_corner[0] < width / 3: 
-                        set_motors(-pwr, pwr)
-                    elif top_left_corner[0] > width * 2 / 3:
-                        set_motors(pwr, -pwr)
-                    else:
-                        set_motors(pwr, pwr)    
+                pwr = portion_of_img
+                print(f"diag_len: {diagonal_len}")
+                print(f"Pwr: {pwr}")
+                if top_left_corner[0] < width / 3:
+                    set_motors(-pwr, pwr)
+                elif top_left_corner[0] > width * 2 / 3:
+                    set_motors(pwr, -pwr)
+                else:
+                    set_motors(pwr, pwr)
 
-                    # if (diagonal_len + follow_proximity_margin) < stop_diagonal_len: # QR code appears smaller -> go forward!
-                    #     if top_left_corner[0] < width / 3: 
-                    #         set_motors(-0.5, 0.5)
-                    #     elif top_left_corner[0] > width * 2 / 3:
-                    #         set_motors(0.5, -0.5)
-                    #     else:
-                    #         set_motors(0.3, 0.3)    
-                    # elif (diagonal_len - follow_proximity_margin) > stop_diagonal_len:
-                    #     set_motors(-0.2, -0.2)
-                    # else:
-                    #     set_motors(0, 0)
-                else: # Stop as soon as we cannot read data anymore
-                    set_motors(0, 0)
-                
         # free camera object and exit
         cap.release()
 
     def stop(self):
         self.running = False
+        self.found_qr = False
--- app/status.py
+++ app/status.py
@@ -5,7 +5,4 @@ def get_status(controller):
     # Example result: b"throttled=0x0\ntemp=44.0'C\nvolt=1.2875V\nvolt=1.2000V\nvolt=1.2000V\nvolt=1.2250V\n"
     status_string = subprocess.check_output(["sh", "get_status.sh"])
 
-    return {
-        "power": controller.power
-    }
-    
+    return {"power": controller.power}
