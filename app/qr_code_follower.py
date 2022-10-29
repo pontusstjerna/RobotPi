65521a0ac2f7288952f06599e2816b84bf565ec0
pontusstjerna <pontus.drejer@gmail.com>
2022-10-29 22:05:26 +0100

    Update qr_following code to be faster and more dynamic

--- app/qr_code_follower.py
+++ app/qr_code_follower.py
@@ -2,75 +2,77 @@ from math import isqrt
 from multiprocessing import Process
 import os
 import cv2
+from pyzbar import pyzbar
 from controller import set_motors
 
 
 def get_dist(a, b):
     return isqrt(int(pow(b[0] - a[0], 2)) + int(pow(b[1] - a[1], 2)))
 
+
 qr_dock_text = os.environ.get("QR_DOCK_TEXT") or "robotpi-dock"
 qr_follow_text = os.environ.get("QR_FOLLOW_TEXT") or "robotpi-follow"
 follow_proximity_margin = 50
 stop_diagonal_len = 150
 min_diagonal_len = 50
+max_no_reading_count = 10
+
+def resize_img(img, scale=0.3):
+    width = int(img.shape[1] * scale)
+    height = int(img.shape[0] * scale)
+    return cv2.resize(img, (width, height))
 
 class QrCodeFollower:
     running = False
     found_qr = False
     diagonal_len = -1
 
-   
     def start(self):
         self.running = True
-        self.process = Process(target=self.__follow__)
+        self.process = Process(target=self.__follow)
         self.process.start()
-    
+
     def stop(self):
         self.running = False
-        
-    def __follow__(self):
+
+    def __follow(self):
         print(f"Following QR code")
 
         # set up camera object
         cap = cv2.VideoCapture(0)
 
-        # QR code detection object
-        detector = cv2.QRCodeDetector()
+        no_reading_count = 0
 
         while self.running:
             # get the image
             _, img = cap.read()
-
-            data, bounding_box, _ = detector.detectAndDecode(img)
+            img = resize_img(img, 0.5)
 
             height, width, _ = img.shape
-            max_diag = get_dist([0, 0], [width, height])
 
-            # if there is a bounding box, draw one, along with the data
-            if bounding_box is not None and self.found_qr:
-                points = bounding_box[0]
-                top_left_corner = points[0]
-                top_right_corner = points[1]
-                diagonal_len = get_dist(points[0], points[2])
+            # Example
+            # [Decoded(data=b'Hej', type='QRCODE', rect=Rect(left=343, top=224, width=78, height=75), polygon=[Point(x=343, y=229), Point(x=348, y=299), Point(x=421, y=294), Point(x=413, y=224)], quality=1, orientation='UP')]
+            qr_reading = pyzbar.decode(img)
 
-                portion_of_img = diagonal_len / max_diag
+            pwr = 0.3
 
-                print(f"Data: {data}")
-                # print(f"Center point: {top_left_corner}")
-                # print(f"Width: {width}, Height: {height}")
+            if len(qr_reading) > 0:
+                no_reading_count = 0
+                decoded = qr_reading[0]
+                diagonal = get_dist(decoded.polygon[0], decoded.polygon[2])
+                
+                movement_of_freedom = width - decoded.rect.width
 
-                print(
-                    f"{'top' if top_left_corner[1] < height / 2 else 'bottom'} {'left' if top_left_corner[0] < width / 2 else 'right'}"
-                )
+                # 0 left, 0.5 forward, 1.0 right
+                turn = decoded.rect.left / movement_of_freedom
+            
+                #print(turn)
+                set_motors(turn, -turn)
 
-                pwr = portion_of_img
-                print(f"diag_len: {diagonal_len}")
-                print(f"Pwr: {pwr}")
-                if top_left_corner[0] < width / 3:
-                    set_motors(-pwr, pwr)
-                elif top_left_corner[0] > width * 2 / 3:
-                    set_motors(pwr, -pwr)
-                else:
-                    set_motors(pwr, pwr)
             else:
-                set_motors(0, 0)
+                no_reading_count += 1
+                if no_reading_count >= max_no_reading_count:
+                    no_reading_count = 0
+                    print("Stopping.")
+                    set_motors(0, 0)
+    
