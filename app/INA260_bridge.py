a48167c5c3c6e284a914da4df451d51a04db3c14
Pontus Drejer <pontus@pythomspace.se>
2024-10-04 10:31:21 +0200

    add charging control

--- app/INA260_bridge.py
+++ app/INA260_bridge.py
@@ -6,9 +6,9 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ina260 = adafruit_ina260.INA260(i2c)
 
 
-def get_current():
+def get_current() -> float:
     return ina260.current
 
 
-def get_voltage():
+def get_voltage() -> float:
     return ina260.voltage
--- app/charge_controller.py
+++ app/charge_controller.py
@@ -1,2 +1,8 @@
+import config
+
+if not config.IS_DEBUG:
+    from INA260_bridge import get_current
+
+
 def is_charging_connected():
-    return False
+    return get_current() < 200
